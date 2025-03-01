a2440ce054a7d64e939b00a5907a32c5fb093174
Nathan Young <yyou9481138@gmail.com>
2025-03-01 11:26:47 -0700

    Added claw example so I can reference it, converting ClawSubsystem from sparkmax to talonFX

--- Robot/src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ Robot/src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -1,22 +1,33 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
+// import com.revrobotics.spark.SparkMax;
+// import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClawConstants;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 public class ClawSubsystem extends SubsystemBase{
+    private TalonFX ClawMotorController;
     private SparkMax motorController;
     private PIDController pidController;
     private RelativeEncoder encoder;
 
+    /* Code that I took from some other team, shouldn't work */
+    // public double getEncoder() {
+    //     return elevatorMotor.getSelectedSensorPosition();
+    // }
+
     public ClawSubsystem() {
         super();
 
-        this.motorController = new SparkMax(ClawConstants.TURN_ID, MotorType.kBrushless);
+        // this.motorController = new SparkMax(ClawConstants.TURN_ID, MotorType.kBrushless);
+        this.ClawMotorController = new TalonFX(ClawConstants.TURN_ID);
+
 
         this.encoder = this.motorController.getEncoder();
 
