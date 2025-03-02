1e4a86a8092e41d20ff7e8e9f1fcd483e3a81095
Ethanscharlie <ethanscharlie@protonmail.com>
2025-03-02 10:38:49 -0700

    Finished Blow Command

---
+++ Robot/src/main/java/frc/robot/commands/SuckNBlowCommands/BlowCommand.java
@@ -0,0 +1,41 @@
+package frc.robot.commands.SuckNBlowCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SuckNBlowSubsystem;
+import frc.robot.subsystems.SuckNBlowSubsystem.OralType;
+
+/** An example command that uses an example subsystem. */
+public class BlowCommand extends Command {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final SuckNBlowSubsystem subsystem;
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public BlowCommand(SuckNBlowSubsystem subsystem) {
+      this.subsystem = subsystem;
+      addRequirements(subsystem);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+      this.subsystem.set(OralType.BLOW);
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() { return true; }
+}
