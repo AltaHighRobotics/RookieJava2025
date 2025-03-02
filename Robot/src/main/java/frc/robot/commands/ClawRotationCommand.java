4099bc70fc3a9fe75bf24b28b2220d45b1aabf57
Nathan Young <yyou9481138@gmail.com>
2025-03-01 18:01:00 -0700

    stupid merge stuff

--- Robot/src/main/java/frc/robot/commands/ClawRotationCommand.java
+++ Robot/src/main/java/frc/robot/commands/ClawRotationCommand.java
@@ -6,32 +6,36 @@ import frc.robot.subsystems.ClawSubsystem;
 
 public class ClawRotationCommand extends Command {
     public enum ClawLevel {
-        LEVEL1,
-        LEVEL2,
-        LEVEL3,
+        POSITION1,
+        POSITION2,
+        POSITION3,
+        POSITION4,
     } 
     
     private ClawLevel clawLevel;
     private ClawSubsystem clawSubsystem;
 
-    public ClawHeightCommand(ClawSubsystem elevatorSubsystem, 
+    public ClawHeightCommand(ClawSubsystem clawSubsystem, 
                                  ClawLevel clawLevel) {
-        this.clawSubsystem = elevatorSubsystem;
-        this.clawLevel = elevatorLevel;
+        this.clawSubsystem = clawSubsystem;
+        this.clawLevel = clawPosition;
         addRequirements(clawSubsystem);
     }
 
     @Override
     public void initialize() {
-        switch (this.clawLevel) {
-            case LEVEL1:
-                this.clawSubsystem.setHeight(ClawConstants.LEVEL1_HEIGHT);
+        switch (this.clawPosition) {
+            case POSITION1:
+                this.clawSubsystem.setHeight(ClawConstants.POSITION1_ROTATION);
                 break;
-            case LEVEL2:
-                this.clawSubsystem.setHeight(ClawConstants.LEVEL2_HEIGHT);
+            case POSITION2:
+                this.clawSubsystem.setHeight(ClawConstants.POSITION2_ROTATION);
                 break;
-            case LEVEL3:
-                this.clawSubsystem.setHeight(ClawConstants.LEVEL3_HEIGHT);
+            case POSITION3:
+                this.clawSubsystem.setHeight(ClawConstants.POSITION3_ROTATION);
+                break;
+            case POSITION4:
+                this.clawSubsystem.setHeight(ClawConstants.POSITION3_ROTATION);
                 break;
         }
     }
