ef6eb58703d21c9a86421b0fb44b17101a2d4c65
Nathan Young <yyou9481138@gmail.com>
2025-03-05 07:59:39 -0700

    Documentation and encoder functions

--- Robot/src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ Robot/src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -17,14 +17,14 @@ public class ClawSubsystem extends SubsystemBase{
         this.ClawMotorController = new TalonFX(ClawConstants.TURN_ID);
 
         /* ENCODER OBJECT EXAMPLE
-        this.encoder = new DutyCycleEncoder(9, 360, 0);
         this.encoder = new Encoder(  int channelA, // DIO port for channel A (blue wire)
                                      int channelB, // DIO port for channel A (yellow wire)
                                      int indexChannel, // DIO port for channel A (green wire)
                                      boolean reverseDirection) //Should be true so that we can detect if the arm is going backwards as well, otherwise we only detect forwards motion
         */
 
-        this.encoder = new Encoder(9, 8, 7, true);
+        this.encoder = new Encoder(9, 8, 7, true); // Plug into DIO ports 9, 8, and 7 (preferabally in colored order above)
+        encoder.setDistancePerPulse(1.0/5.7); // 360 divided by the 2048 pulses per rotation should mean every 5.7 pulses is one rotational degree
 
         final double P = ClawConstants.P;
         final double I = ClawConstants.I;
@@ -43,7 +43,15 @@ public class ClawSubsystem extends SubsystemBase{
         return encoder.get();
     }
 
+    public double getDistanceTraveled() {
+        return encoder.getDistance(); // Will return how much the encoder is turnned
+    }
+
     public void resetRotation() {
-        encoder.reset();        // Reset the Encoder distance to zero
+        encoder.reset();        // Reset the Encoder distance to zero (a good idea to trigger when the claw is initialized)
+    }
+
+    public double checkIfStopped() {
+        return encoder.getStopped(); //Checks if encoder is currently stopped
     }
 }
