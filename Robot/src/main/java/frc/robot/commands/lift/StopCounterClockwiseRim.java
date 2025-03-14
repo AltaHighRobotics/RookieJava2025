f1773a1293d913b21edb75cf1a93b4dcfe7da415
Duncan <143680478+Tiny0pus@users.noreply.github.com>
2025-03-14 09:29:08 -0600

    whatever,  it doesnt work

---
+++ Robot/src/main/java/frc/robot/commands/lift/StopCounterClockwiseRim.java
@@ -0,0 +1,29 @@
+package frc.robot.commands.lift;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.RimmerSubsystem;
+import frc.robot.subsystems.RimmerSubsystem.MoveStyle;
+
+/** An example command that uses an example subsystem. */
+public class StopCounterClockwiseRim extends Command {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final RimmerSubsystem subsystem;
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public StopCounterClockwiseRim(RimmerSubsystem subsystem) {
+      this.subsystem = subsystem;
+      addRequirements(subsystem);
+    }
+
+    /**
+     * Blows for a few seconds to shoot the ball and then stops the motor
+     */
+    @Override
+    public void execute() {
+      this.subsystem.set(MoveStyle.STOP);
+    }
+}
